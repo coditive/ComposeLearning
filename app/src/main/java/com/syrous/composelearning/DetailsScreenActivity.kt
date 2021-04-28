be5b297ab50362f2610dd5736e059f0c1104ba40
coditive <syrouscodive@gmail.com>
2021-04-28 21:03:39 +0530

    Detail Layout Completed

--- app/src/main/java/com/syrous/composelearning/DetailsScreenActivity.kt
+++ app/src/main/java/com/syrous/composelearning/DetailsScreenActivity.kt
@@ -7,16 +7,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.android.material.bottomappbar.BottomAppBar
@@ -38,13 +37,80 @@ class DetailsScreenActivity: AppCompatActivity() {
 @Composable
 fun DetailsScreenView() {
     Scaffold(
-        bottomBar = { BottomAppBar() }
-    ) {
-
-    }
+        bottomBar = { BottomAppBar() },
+        content = { BodyContent() }
+    )
 }
 
+@Composable
+fun BodyContent() {
+   Column(
+       modifier = Modifier
+           .fillMaxSize()
+   ) {
+       BoxWithConstraints (
+           modifier = Modifier
+               .wrapContentSize()
+       ){
+           Image(
+               painter = painterResource(id = R.drawable.image_6),
+               contentDescription = null,
+               modifier = Modifier
+                   .fillMaxWidth()
+                   .fillMaxHeight(0.6f),
+               contentScale = ContentScale.FillBounds
+           )
 
+           Icon(
+               painter = painterResource(id = R.drawable.ic_baseline_chevron_left_24),
+               contentDescription = null,
+               modifier = Modifier
+                   .padding(start = 30.dp, top = 40.dp),
+               tint = Color.Black
+           )
+
+           LinearProgressIndicator(
+               progress = 0.4f,
+               modifier = Modifier
+                   .padding(
+                       top = (constraints.maxHeight * .2f).dp,
+                       start = (constraints.maxWidth * .08f).dp
+                   )
+                   .height(2.dp),
+               color = Color(0xFFFFFFFF),
+               backgroundColor = Color(0xABFFFFFF)
+           )
+
+       }
+
+       Text(
+           text = "HISAKO",
+           fontSize = 24.sp,
+           letterSpacing = 10.sp,
+           modifier = Modifier
+               .padding(top = 40.dp, start = 30.dp),
+           color = Color.Black
+       )
+
+       Text(
+           text = "$249",
+           fontSize = 24.sp,
+           modifier = Modifier
+               .padding(top = 30.dp, start = 30.dp),
+           color = Color.Black
+       )
+       
+       Text(
+           text = "Named after asteroid 6 0 9 4 (h i s a k o) \nis currently travelling through time and \nspace.",
+           fontSize = 16.sp,
+           modifier = Modifier
+               .wrapContentSize()
+               .padding(top = 20.dp, start = 30.dp),
+           color = Color.Black
+       )
+   }
+
+}
 
 @Preview
 @Composable
